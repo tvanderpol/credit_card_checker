ed6565476f9f8f86e1d98bb192294072d563458a
Thomas van der Pol <thomasvanderpol@gmail.com>
2012-07-10 12:36:46 +1000

    Naieve implementation of type checking done

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,5 +1,49 @@
 class CreditCard
-  def init()
-    puts "Heyo!"
+  attr_reader :number, :type
+  
+  def initialize(number)
+    @number = number
+    @num_array = @number.to_s.scan(/\d/).map { |x| x.to_i }
+    @type = "INVALID"
+    set_type
+  end
+  
+  private
+  
+  def set_type
+    case @num_array.length
+      when 13
+        validate_visa
+      when 15
+        validate_amex
+      when 16
+        validate_visa
+        validate_discover
+        validate_mastercard        
+    end
+  end
+  
+  def validate_amex
+    validation_numbers = @num_array[0..1]
+    if validation_numbers == [3, 4] || validation_numbers == [3, 7]
+      @type = "AMEX" 
+    end
+  end
+  
+  def validate_discover
+    validation_numbers = @num_array[0..3]
+    @type = "Discover" if validation_numbers == [6,0,1,1]
+  end
+  
+  def validate_mastercard
+    validation_numbers = @num_array[0..1]
+    if validation_numbers == [5, 1] || validation_numbers == [5, 5]
+      @type = "MasterCard"
+    end
+  end
+
+  def validate_visa
+    validation_number = @num_array[0]
+    @type = "Visa" if validation_number == 4
   end
 end
--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -1,8 +1,28 @@
 require_relative '../lib/main'
 
 describe CreditCard do
-  it "should take a Credit Card number as init argument" do
+  it "should take a Credit Card number as initial argument" do
     cc = CreditCard.new(4111111111111111)
     cc.kind_of?(CreditCard).should be_true
   end
+  
+  it "should set type to AMEX when the number begins with 34 or 37 and is 15 digits long" do
+    cc = CreditCard.new(378282246310005)
+    cc.type.should eql 'AMEX'
+  end
+  
+  it "should set type to Discover when the number begins with 6011 and is 16 digits long" do
+    cc = CreditCard.new(6011111111111117)
+    cc.type.should eql 'Discover'
+  end
+
+  it "should set type to MasterCard when the number begins with 51 or 55 and is 16 digits long" do
+    cc = CreditCard.new(5105105105105100)
+    cc.type.should eql 'MasterCard'
+  end
+
+  it "should set type to Visa when the number begins with 4 and is 13 or 16 digits long" do
+    cc = CreditCard.new(4111111111111111)
+    cc.type.should eql 'Visa'
+  end  
 end
